e548f2d343fa118699a33e7fa06ab2a783647967
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2018-03-11 00:13:58 +0530

    Few more fixes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import configuration from "./configuration";
 import express from "express";
-import ExpressRotuer from "./exxpress.router";
+import ExpressRotuer from "./express.router";
+
 
 let app = express();
+let expresRoutes = new ExpressRotuer(app);
+expresRoutes.setRoutes();
 
 app.listen(configuration.port, () => {
 	console.log(`Express server app listening on port ${configuration.port}!`)
