7476377cb5d50491a819c0d72e0020b7236f4ddf
Tym17 <Tym17@users.noreply.github.com>
2019-12-12 14:11:09 +0100

    Fixed a small typo on ExpressRouter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import ExpressRotuer from './express.router';
+import ExpressRouter from './express.router';
 
 dotenv.config();
 
 const app = express();
-const expressRoutes = new ExpressRotuer(app);
+const expressRoutes = new ExpressRouter(app);
 expressRoutes.init();
 
 app.listen(process.env.PORT, () => {
